417e534d900c409b16a26953c3570aa3026e3828
nddungshinigami179-boop <nddung.shinigami179@gmail.com>
2026-09-13 10:53:20 +0700

    api

--- index.js
+++ index.js
@@ -152,3 +152,17 @@ function logout() {
             "login.html";
     }
 }
+
+fetch('https://PROJECT_TOKEN.mockapi.io/users/1/tasks', {
+  method: 'GET',
+  headers: {'content-type':'application/json'},
+}).then(res => {
+  if (res.ok) {
+      return res.json();
+  }
+  // handle error
+}).then(tasks => {
+  // Do something with the list of tasks
+}).catch(error => {
+  // handle error
+})
